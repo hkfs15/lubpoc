09075ab423e786f4d2128a976237678eb50a1f5d
hkfs15 <hkfs15@163.com>
2015-10-14 10:03:37 +0000

    Add UML diagram; add static templates

--- lubpoc/settings/base.py
+++ lubpoc/settings/base.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'lubpoc.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -97,3 +97,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,"static"),
+    )
